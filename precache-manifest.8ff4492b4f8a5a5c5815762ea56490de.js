bf4dd046e6067e072f9836ef137098b27523181c
nazarielle <nazarielle@gmail.com>
2019-09-14 23:14:51 -0400

    add build files

---
+++ precache-manifest.8ff4492b4f8a5a5c5815762ea56490de.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98eac904a4efde27764c082efb0513d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98faef436b76b78089c3",
+    "url": "/static/js/2.a23e07d6.chunk.js"
+  },
+  {
+    "revision": "9f6722743e3c830a26c9",
+    "url": "/static/js/main.2b6a7cb4.chunk.js"
+  },
+  {
+    "revision": "a1db24ebb0df2b4d85f0",
+    "url": "/static/js/runtime~main.e7551886.js"
+  }
+]);
